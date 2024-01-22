6947d873e7a1b0ab2b520cced4918c12eb55ef0f
sasha <sashaAriapor@gmail.com>
2024-01-22 17:38:50 +0330

    write login route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post, BadRequestException } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { UserService } from 'src/user/user.service';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -15,5 +16,14 @@ export class AuthController {
     const checkExit = await this.userService.findUserByEmail(dto.email);
     if (checkExit) throw new BadRequestException("User Already Exited");
     return await this.userService.createUser(dto);
+  }
+
+  @Post("/login")
+  async login(@Body() dto: LoginDto) {
+    const user = await this.userService.findUserByEmail(dto.email);
+    if(!user) throw new BadRequestException("Email Or Password is Wrong");
+    const checkPassword = await this.authService.checkPassword(user.hash, dto.password);
+    if(!checkPassword) throw new BadRequestException("Email Or Password is Wrong");
+    return user;
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import * as argon2 from "argon2";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    async checkPassword(hash: string, password: string) {
+        return await argon2.verify(hash, password);
+    }
+}
