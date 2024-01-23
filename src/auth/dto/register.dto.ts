f09029a87d25418522707f79fd04b1cec9afc5d2
sasha <sashaAriapor@gmail.com>
2024-01-23 16:18:18 +0330

    validation part

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,6 +1,22 @@
+import { IsEmail, IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
+
 export class RegisterDto {
+  @IsString()
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(16)
   firstname: string;
+
+  @IsString()
   lastname?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
   password: string;
 }
